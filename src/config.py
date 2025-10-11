159db92f5bc77bcc9fa30f06b865a07c9e687ed1
jedg83 <79536920+jedg83@users.noreply.github.com>
2025-10-11 17:25:35 +0000

    .

--- src/config.py
+++ src/config.py
@@ -3,4 +3,4 @@ from datetime import date
 LANGUAGE = "en"
 MAX_RESULTS = 20
 TOPICS = ["money laundering", "terrorist financing", "corruption"]
-OUTPUT_FILE = "data/news_intelligence_{date.today()}.json"
+OUTPUT_FILE = f"data/news_intelligence_{date.today()}.json"
