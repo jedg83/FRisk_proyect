72c41c4da0774266b4a7a709d7a0eeff11c40598
jedg83 <79536920+jedg83@users.noreply.github.com>
2025-10-11 17:22:46 +0000

    adding different name to exported file

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 # Configuration for your AI News Agent
-
+from datetime import date
 LANGUAGE = "en"
 MAX_RESULTS = 20
 TOPICS = ["money laundering", "terrorist financing", "corruption"]
-OUTPUT_FILE = "data/news_intelligence.json"
+OUTPUT_FILE = "data/news_intelligence_{date.today()}.json"
